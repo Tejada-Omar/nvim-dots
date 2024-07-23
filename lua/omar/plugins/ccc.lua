603dfff448e9503a3d91da100123ec30e6929b98
Omar Tejada <tejada.omar03@gmail.com>
2024-07-23 01:15:16 -0600

    feat(ccc): enable color highlighting by toggle

--- lua/omar/plugins/ccc.lua
+++ lua/omar/plugins/ccc.lua
@@ -6,15 +6,24 @@ return {
       highlight_mode = 'virtual',
       preserve = true,
       highlighter = {
-        auto_enable = true,
+        auto_enable = false,
       },
     },
     keys = {
       { '<leader>cc', '<CMD>CccPick<CR>', desc = 'Open color picker' },
-      { '<leader>cr', function()
-        vim.cmd.CccHighlighterToggle()
-        vim.cmd.CccHighlighterToggle()
-      end, desc = 'Reload color highlighting' },
+      {
+        '<leader>ct',
+        '<CMD>CccHighlighterToggle',
+        desc = 'Toggle color highlighter',
+      },
+      {
+        '<leader>cr',
+        function()
+          vim.cmd.CccHighlighterToggle()
+          vim.cmd.CccHighlighterToggle()
+        end,
+        desc = 'Reload color highlighting',
+      },
     },
   },
 }
