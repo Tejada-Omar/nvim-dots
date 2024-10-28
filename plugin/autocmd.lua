cb1505ae7a9aab1580718af3768901dc960cb5cf
Omar Tejada <tejada.omar03@gmail.com>
2024-10-27 20:57:59 -0600

    fix(sugar): disable inlay hints
    Messes with range-wise commands and block-mode

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -2,22 +2,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank { higroup = 'IncSearch' } end,
 })
 
-vim.api.nvim_create_autocmd({ 'LspAttach', 'InsertEnter', 'InsertLeave' }, {
-  callback = function(args)
-    local enabled = args.event ~= 'InsertEnter'
-    if
-      vim.tbl_isempty(vim.lsp.get_clients {
-        bufnr = 0,
-        method = vim.lsp.protocol.Methods.textDocument_inlayHint,
-      })
-    then
-      enabled = false
-    end
-
-    vim.lsp.inlay_hint.enable(enabled, { bufnr = 0 })
-  end,
-})
-
 vim.api.nvim_create_autocmd('TermEnter', {
   callback = function(_)
     vim.cmd('startinsert')
