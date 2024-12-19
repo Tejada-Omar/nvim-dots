d4a183377ca13b6a84931a7ef3ca7d99c6111ec8
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2024-12-18 21:26:06 -0700

    fix(ts): merge treesitter plugin specs

--- lua/omar/plugins/treesitter.lua
+++ lua/omar/plugins/treesitter.lua
@@ -11,55 +11,54 @@ return {
         opts = { enable_autocmd = false },
       },
     },
-    init = function() vim.g.skip_ts_context_commentstring_module = true end,
-    opts = function()
+    init = function()
+      vim.g.skip_ts_context_commentstring_module = true
       vim.o.foldmethod = 'expr'
       vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-
-      return {
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = { 'latex' }, -- For vimtex
-        },
-        incremental_selection = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ic'] = '@class.inner',
-              ['ac'] = '@class.outer',
-              ['iC'] = '@call.inner',
-              ['aC'] = '@call.outer',
-              ['il'] = '@loop.inner',
-              ['al'] = '@loop.outer',
-              ['ii'] = '@conditional.inner',
-              ['ai'] = '@conditional.outer',
-              ['iP'] = '@parameter.inner',
-              ['aP'] = '@parameter.outer',
-            },
-            selection_modes = {
-              ['@parameter.outer'] = 'v',
-              ['@function.outer'] = 'V',
-              ['@class.outer'] = '<c-v>',
-            },
-            include_surrounding_whitespace = false,
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
-          },
-        },
-      }
     end,
+    opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = { 'latex' }, -- For vimtex
+      },
+      incremental_selection = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ic'] = '@class.inner',
+            ['ac'] = '@class.outer',
+            ['iC'] = '@call.inner',
+            ['aC'] = '@call.outer',
+            ['il'] = '@loop.inner',
+            ['al'] = '@loop.outer',
+            ['ii'] = '@conditional.inner',
+            ['ai'] = '@conditional.outer',
+            ['iP'] = '@parameter.inner',
+            ['aP'] = '@parameter.outer',
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'V',
+            ['@class.outer'] = '<c-v>',
+          },
+          include_surrounding_whitespace = false,
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>A'] = '@parameter.inner',
+          },
+        },
+      },
+    },
     config = function(_, opts) require('nvim-treesitter.configs').setup(opts) end,
   },
 }
