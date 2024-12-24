89a791c875dab5e07516e19bcd04bde32b6a46e0
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2024-12-24 08:50:14 -0700

    feat(git): better behavior in terminal buffers

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -2,21 +2,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank { higroup = 'IncSearch' } end,
 })
 
-vim.api.nvim_create_autocmd('TermEnter', {
-  callback = function(_)
-    vim.cmd('startinsert')
-
-    vim.o.number = false
-    vim.o.relativenumber = false
-  end,
-})
-
-vim.api.nvim_create_autocmd('TermLeave', {
-  callback = function(_)
-    vim.o.number = true
-    vim.o.relativenumber = true
-  end,
-})
+vim.api.nvim_create_autocmd('TermEnter', { command = 'startinsert' })
+vim.api.nvim_create_autocmd(
+  'TermOpen',
+  { command = 'setlocal nonumber norelativenumber signcolumn=no' }
+)
 
 vim.api.nvim_create_autocmd({ 'BufNew', 'BufWritePost' }, {
   callback = function(_)
