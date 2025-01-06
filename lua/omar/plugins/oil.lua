c9a63e2e4b81cd112e06e200726588be0ea3ee65
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-01-06 04:22:10 -0700

    feat(dir): re-add neotree config
    Seems helpful for new codebases after all

--- lua/omar/plugins/oil.lua
+++ lua/omar/plugins/oil.lua
@@ -29,4 +29,42 @@ return {
       },
     },
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      { 'nvim-tree/nvim-web-devicons', optional = true },
+    },
+    keys = {
+      {
+        '<leader>el',
+        '<CMD>Neotree toggle reveal<CR>',
+        desc = 'Toggle Neotree',
+      },
+      {
+        '<leader>er',
+        '<CMD>Neotree reveal<CR>',
+        desc = 'Focus Neotree on current file',
+      },
+      {
+        '<leader>eR',
+        '<CMD>Neotree show<CR>',
+        desc = 'Open Neotree',
+      },
+    },
+    cmd = 'Neotree',
+    opts = {
+      sources = {
+        'filesystem',
+        'document_symbols'
+      },
+      filesystem = {
+        follow_current_file = {
+          enable = true,
+          leave_dirs_open = true,
+        },
+      },
+    },
+  },
 }
