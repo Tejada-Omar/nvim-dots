72b3d5ebef8bb8fea25866dc4e87656bded679cf
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-01-12 05:13:02 -0700

    feat(zen): allow only centering buffer

--- lua/omar/plugins/snacks.lua
+++ lua/omar/plugins/snacks.lua
@@ -7,14 +7,31 @@ return {
     },
     keys = {
       {
-        '<leader>z',
+        '<leader>zz',
         function() require('snacks.zen').zen() end,
         desc = 'Toggle zen mode',
       },
       {
-        '<leader>Z',
+        '<leader>zo',
         function() require('snacks.zen').zoom() end,
         desc = 'Toggle zoom mode',
+      },
+    },
+  },
+  {
+    'shortcuts/no-neck-pain.nvim',
+    opts = {
+      buffers = {
+        wo = {
+          fillchars = 'eob: ',
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>zn',
+        '<CMD>NoNeckPain<CR>',
+        desc = 'Center buffer',
       },
     },
   },
