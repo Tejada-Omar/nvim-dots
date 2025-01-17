2462e2b5c9432471a5b8271b3940aef3d3314a91
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-01-17 16:28:54 -0700

    refactor(java): remove unused binds

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,16 +1,1 @@
 vim.bo.textwidth = 120
-
-local run_maven = function()
-  local root_dir = vim.fs.dirname(
-    vim.fs.find({ '.gradlew', '.git', 'mvnw' }, { upward = true })[1]
-  )
-
-  vim.cmd([[9TermExec cmd='mvn javafx:run' dir=]] .. root_dir)
-end
-
-vim.keymap.set(
-  'n',
-  '<leader>tj',
-  function() run_maven() end,
-  { desc = 'Run maven javafx:run' }
-)
