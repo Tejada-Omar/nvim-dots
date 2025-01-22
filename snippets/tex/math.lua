3d3644a7272ef509519d19380e6a1144c11d512f
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-01-22 11:11:31 -0700

    snip(tex): add more math snippets

--- snippets/tex/math.lua
+++ snippets/tex/math.lua
@@ -33,6 +33,41 @@ return {}, {
     show_condition = in_mathzone,
   }, t('\\vDash')),
 
+  s({
+    trig = ';rr',
+    desc = 'Insert \\var',
+    condition = in_mathzone,
+    show_condition = in_mathzone,
+  }, fmta('\\var{<>}<>', { i(1), i(0) })),
+
+  s({
+    trig = ';rf',
+    desc = 'Insert \\forall',
+    condition = in_mathzone,
+    show_condition = in_mathzone,
+  }, t('\\forall')),
+
+  s({
+    trig = ';rF',
+    desc = 'Insert \\forall',
+    condition = in_mathzone,
+    show_condition = in_mathzone,
+  }, t('\\forall')),
+
+  s(
+    {
+      trig = ';rs',
+      desc = 'Insert a super and subscript',
+      condition = in_mathzone,
+      show_condition = in_mathzone,
+    },
+    fmta('<>_<>^<>', {
+      i(1, 'f'),
+      i(2, 'k'),
+      i(0, 'n'),
+    })
+  ),
+
   s(
     {
       trig = ';sl',
@@ -41,6 +76,20 @@ return {}, {
       show_condition = in_mathzone,
     },
     fmta('<>_1, \\dots, <>_<>', {
+      i(1, 'x'),
+      rep(1),
+      i(2, 'n'),
+    })
+  ),
+
+  s(
+    {
+      trig = ';sL',
+      desc = 'Create math list starting from zero',
+      condition = in_mathzone,
+      show_condition = in_mathzone,
+    },
+    fmta('<>_0, \\dots, <>_{<>-1}', {
       i(1, 'x'),
       rep(1),
       i(2, 'n'),
