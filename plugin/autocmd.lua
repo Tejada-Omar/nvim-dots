32ac9d790656d0d4212f6a79fbfc60a1e6e542df
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-06-25 23:39:51 -0600

    perf(lsp): kill all lsp clients on close

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -37,3 +37,9 @@ vim.api.nvim_create_autocmd('FileType', {
     )
   end,
 })
+
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  callback = function(_)
+    vim.iter(vim.lsp.get_clients()):each(function(c) c:stop() end)
+  end,
+})
