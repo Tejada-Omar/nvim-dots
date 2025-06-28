401548b2ec465c3e96a8978cd42fa19be1db27c1
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-06-28 05:37:15 -0600

    feat(snacks): enable more snacks modules

--- lua/omar/plugins/snacks.lua
+++ lua/omar/plugins/snacks.lua
@@ -2,10 +2,27 @@ return {
   {
     'folke/snacks.nvim',
     lazy = false,
-    opts = {
-      bigfile = { enabled = true },
-    },
+    priority = 1000,
+    opts = function()
+      -- Toggle the profiler
+      Snacks.toggle.profiler():map('<leader>pp')
+      -- Toggle the profiler highlights
+      Snacks.toggle.profiler_highlights():map('<leader>ph')
+
+      return {
+        bigfile = { enabled = true },
+        dim = { enabled = true },
+        debug = { enabled = true },
+        profiler = { enabled = true },
+        picker = { enabled = true },
+      }
+    end,
     keys = {
+      {
+        '<leader>ps',
+        function() Snacks.profiler.scratch() end,
+        desc = 'Profiler scratch buffer',
+      },
       {
         '<leader>zz',
         function() require('snacks.zen').zen() end,
@@ -17,6 +34,23 @@ return {
         desc = 'Toggle zoom mode',
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...) Snacks.debug.inspect(...) end
+          _G.bt = function() Snacks.debug.backtrace() end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+          -- Create some toggle mappings
+          Snacks.toggle.diagnostics():map('<leader>ud')
+          Snacks.toggle.treesitter():map('<leader>uT')
+          Snacks.toggle.inlay_hints():map('<leader>uh')
+          Snacks.toggle.dim():map('<leader>uD')
+        end,
+      })
+    end,
   },
   {
     'shortcuts/no-neck-pain.nvim',
