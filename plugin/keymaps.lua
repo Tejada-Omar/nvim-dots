0da32ea54cb8df8342827a1ed7ebc6984c5d1565
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-07-08 11:06:13 -0600

    feat(sess): use builtin session management
    Hopefully this will help the slowdown on my laptop when hitting IO

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,1 +1,35 @@
 vim.keymap.set('i', 'jk', '<Esc>')
+
+vim.keymap.set('n', '<leader>fs', function()
+  local fzf = require('fzf-lua')
+  fzf.files {
+    fd_opts = '-c never -tf -tl -d 1 -e vim -E .git -E node_modules',
+    actions = {
+      ['enter'] = function(sel) vim.cmd.source(sel[1]) end,
+    },
+  }
+end, { desc = 'Load session' })
+
+vim.keymap.set('n', '<leader>fS', function()
+  vim.ui.input({
+    prompt = 'Session Name',
+  }, function(sel)
+    if sel == nil then return end
+
+    sel = vim.trim(sel)
+    local file = sel ~= '' and 'Session-' .. sel .. '.vim' or 'Session.vim'
+
+    if not vim.uv.fs_stat(file) then
+      vim.cmd.mksession(file)
+      return
+    end
+
+    vim.ui.select(
+      { 'Overwrite', 'Abort' },
+      { prompt = 'File found. Action? ' },
+      function(_, idx)
+        if idx == 1 then vim.cmd.mksession { file, bang = true } end
+      end
+    )
+  end)
+end, { desc = 'Save session' })
