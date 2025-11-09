653109124b644a1391aa70f3f91e6b5275e4b105
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-11-08 22:40:49 -0700

    feat(status): clean up redundant info

--- lua/omar/plugins/statusline.lua
+++ lua/omar/plugins/statusline.lua
@@ -2,6 +2,41 @@ return {
   {
     'nvim-mini/mini.statusline',
     init = function() vim.o.showmode = false end,
-    config = true,
+    opts = function()
+      local mini = require('mini.statusline')
+      local mode, mode_hl = mini.section_mode { trunc_width = 120 }
+      local git = mini.section_git { trunc_width = 40 }
+      local filename = mini.section_filename { trunc_width = 140 }
+      local fileinfo = mini.section_fileinfo { trunc_width = 120 }
+      local location = mini.section_location { trunc_width = 75 }
+      local search = mini.section_searchcount { trunc_width = 75 }
+
+      return {
+        content = {
+          active = function()
+            return mini.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+          inactive = function()
+            return mini.combine_groups {
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+        },
+
+        use_icons = false,
+      }
+    end,
   },
 }
